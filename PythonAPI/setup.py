8b51f05302b2984740fd668a24258f6b2c70d529
Paul Huang <aphroteus@gmail.com>
2019-09-28 07:14:53 +0800

    For MSVC C compiler, remove invalid args
    Build and install
    python setup.py build_ext install

--- PythonAPI/setup.py
+++ PythonAPI/setup.py
@@ -1,17 +1,27 @@
 from setuptools import setup, Extension
 import numpy as np
+import platform
 
 # To compile and install locally run "python setup.py build_ext --inplace"
 # To install library to Python site-packages run "python setup.py build_ext install"
 
-ext_modules = [
-    Extension(
-        'pycocotools._mask',
-        sources=['../common/maskApi.c', 'pycocotools/_mask.pyx'],
-        include_dirs = [np.get_include(), '../common'],
-        extra_compile_args=['-Wno-cpp', '-Wno-unused-function', '-std=c99'],
-    )
-]
+if platform.system() == 'Windows':
+    ext_modules = [
+        Extension(
+            'pycocotools._mask',
+            sources=['../common/maskApi.c', 'pycocotools/_mask.pyx'],
+            include_dirs = [np.get_include(), '../common'],
+        )
+    ]
+else:
+    ext_modules = [
+        Extension(
+            'pycocotools._mask',
+            sources=['../common/maskApi.c', 'pycocotools/_mask.pyx'],
+            include_dirs = [np.get_include(), '../common'],
+            extra_compile_args=['-Wno-cpp', '-Wno-unused-function', '-std=c99'],
+        )
+    ]
 
 setup(
     name='pycocotools',
